f7886e47f5b1a96612e56c49335684647f1ba932
Gabriel Angelo <gabriel_teodoro123@hotmail.com>
2025-01-24 12:32:50 -0300

    Adicionando função exibir lista amigos

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ function adicionarAmigo(){
     let nome = document.getElementById('amigo').value;
     if (nome){
         amigos.push(nome);
-
         document.getElementById('amigo').value = '';
-
+        mostrarAmigos();
         console.log(amigos);
     } else {
         alert('Por favor, insira um nome válido')
     }
+
+}
+
+function mostrarAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+    amigos.forEach(function(amigo){
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        lista.appendChild(item);
+    });
 
 }
 
